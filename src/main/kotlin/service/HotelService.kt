a31b633c768f3b2bbf712d7774e45a6f9d2cbfd0
Brynjar-l <brynjar.sighvatsson@gmail.com>
2025-04-07 15:18:56 +0000

    feat(HotelService): add queryCache() to query Hotels based on an inputted SearchCriteria object.
    Signed-off-by: Brynjar-l <brynjar.sighvatsson@gmail.com>

--- src/main/kotlin/service/HotelService.kt
+++ src/main/kotlin/service/HotelService.kt
@@ -1,42 +1,49 @@
 package ice.private.brynj.service
 
-import ice.private.brynj.model.*
 import ice.private.brynj.database.entities.*
 import ice.private.brynj.model.Amenity
 import ice.private.brynj.model.Hotel
+import ice.private.brynj.utils.SearchCriteria
 import org.jetbrains.exposed.sql.transactions.transaction
 import java.time.LocalDate
 
-data class SearchCriteria(
-    var selectedAmenities: Set<Amenity> = emptySet(),
-    var city: String? = null,
-    var minStarRating: Double? = null,
-    var maxStarRating: Double? = null,
-    var minPricePerNight: Int? = null,
-    var maxPricePerNight: Int? = null,
-    var fromDate: LocalDate? = null,
-    var toDate: LocalDate? = null,
-    var numberOfBeds: Int? = null,
-)
+
 
 
 object HotelService {
 
     private val cache = LinkedHashSet<Hotel>()
-    private fun loadHotelsIntoCache(limit: Int) = transaction { cache.addAll(HotelEntity.all().limit(limit).map { it.toDto() }) }
+
+    init { cacheMiss(5000) }
 
 
     fun searchHotels(criteria: SearchCriteria): List<Hotel> {
+        /* leita fyrst í cache */
+        val resultList = queryCache(criteria)
 
+        /* skila result ef match fannst í cache, annars cacheMiss */
+        if (resultList.isNotEmpty()) return resultList
+        else cacheMiss()
+
+        /* skilar tómum lista ef ekkert finnst */
+        return queryCache(criteria)
+    }
+
+
+    private fun cacheMiss(sizeLimit: Int = 5000) = transaction {
+        cache.addAll(HotelEntity.all().limit(sizeLimit).map { it.toDto() })
+    }
+
+    private fun queryCache(criteria: SearchCriteria): List<Hotel> {
         /** byrjum á að leita í in-memory cache
-        *  .filter er 'Higher-Order Function', það tekur lista (eins og Cache) og iteratar í gegnum hann
-        *  þar sem '{ hotel -> ' táknar eitt 'item' í listanum. allt sem er innan { } virkar svipar
-        *  og for lykkja, þar sem ef lokagildið er 'true', þá bætist 'itemið' við í nýja listann sem .filter býr til.*/
-        val resultList = cache.filter { hotel ->
+         *  .filter er 'Higher-Order Function', það tekur lista (eins og Cache) og iteratar í gegnum hann
+         *  þar sem '{ hotel -> ' táknar eitt 'item' í listanum. allt sem er innan { } virkar svipar
+         *  og for lykkja, þar sem ef lokagildið er 'true', þá bætist 'itemið' við í nýja listann sem .filter býr til.*/
+        return cache.filter { hotel ->
 
             /** Ef 'city' er null, þá skilar það true. Annars athugar það hvort 'hotel'.city er jafnt of 'criteria'.city
-            *  hotelið verður ekki bætt við í lokalistann ef það fær 'false', þess vegna fá öll hótel 'true'
-            *  ef criteriaði tilgreinir ekki 'city'                  */
+             *  hotelið verður ekki bætt við í lokalistann ef það fær 'false', þess vegna fá öll hótel 'true'
+             *  ef criteriaði tilgreinir ekki 'city'                  */
             val matchesCity = criteria.city?.equals(hotel.city, ignoreCase = true) ?: true
 
             /** kannar hvort 'hotel'.starRating sé fyrir utan criteria mörkin, og skilar 'true' ef svo er ekki. */
@@ -70,25 +77,24 @@ object HotelService {
 
             /*  */
             val matchesDate = if (criteria.fromDate != null && criteria.toDate != null) {
-                TODO()
+                hotelCheckAvailability(hotel, criteria.fromDate!!, criteria.toDate!!)
             } else true
 
 
             /* þetta expression ákveður hvort 'hotelið' sem við erum á núna fari í lokalistann */
             matchesCity && matchesStarRating && matchesAmenities && matchesPrice && matchesDate
         }
-
-
-        /* skila result ef það fannst í cache */
-        if (resultList.isNotEmpty()) return resultList
-
-
     }
 
+    /* athugar bara hvort það sé að minnsta kosti eitt herbergi sem er laust innan tímarammans */
+    private fun hotelCheckAvailability(hotel: Hotel, fromDate: LocalDate, toDate: LocalDate): Boolean {
+        return hotel.rooms.any { room ->
+            val isOverlapping = room.bookedDates?.any { (bookedFromDate, bookedToDate) ->
+                fromDate <= bookedToDate && toDate >= bookedFromDate
+            } ?: false
 
-
-
-    fun createHotel() {}
-    fun deleteHotel() {}
+            !isOverlapping
+        }
+    }
 }
 
