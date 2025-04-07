7245e6a6d9e9efc1ab4678dde00802f06be0fd8c
Brynjar <brynjar.sighvatsson@gmail.com>
2025-04-07 15:17:10 +0000

    feat: create SearchCriteria.kt
    SearchCriteria is a data class used to pass a 'Query Object' between frontend and backend.
    
    Signed-off-by: Brynjar-l <brynjar.sighvatsson@gmail.com>

---
+++ src/main/kotlin/utils/SearchCriteria.kt
@@ -0,0 +1,16 @@
+package ice.private.brynj.utils
+
+import ice.private.brynj.model.Amenity
+import java.time.LocalDate
+
+data class SearchCriteria(
+    var selectedAmenities: Set<Amenity> = emptySet(),
+    var city: String? = null,
+    var minStarRating: Double? = null,
+    var maxStarRating: Double? = null,
+    var minPricePerNight: Int? = null,
+    var maxPricePerNight: Int? = null,
+    var fromDate: LocalDate? = null,
+    var toDate: LocalDate? = null,
+    var numberOfBeds: Int? = null,
+)
