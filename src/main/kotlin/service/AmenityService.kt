ba8b304ad29c532c2b70edc281c8acd85f5cdaf0
Brynjar-l <brynjar.sighvatsson@gmail.com>
2025-04-09 14:08:15 +0000

    refactor: move TypeAlias definition into separate file

--- src/main/kotlin/service/AmenityService.kt
+++ src/main/kotlin/service/AmenityService.kt
@@ -3,14 +3,9 @@ package service
 import database.entities.AmenityEntity
 import model.Amenity
 import org.jetbrains.exposed.sql.transactions.transaction
+import utils.AmenityProvider
+import utils.defaultAmenityProvider
 
-typealias AmenityProvider = () -> List<Amenity>
-
-fun defaultAmenityProvider(): AmenityProvider = {
-    transaction {
-        AmenityEntity.all().map { it.toDto() }
-    }
-}
 
 class AmenityService(
     private val amenityProvider: AmenityProvider = defaultAmenityProvider()
